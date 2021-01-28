dd377fcd34a4b8042b09a0501a27b271f77bb887
mjtillman <mjtillman@gmail.com>
2021-01-28 10:53:18 -0600

    finished methods exercise

---
+++ hclfresher/src/phase1/callMethod.java
@@ -0,0 +1,18 @@
+package phase1;
+
+//call by value
+public class callMethod {
+
+  static int val = 150;
+
+  static int operation(int val) {
+    val = val * 10 / 100;
+    return (val);
+  }
+
+  public static void main(String args[]) {
+    System.out.println("Before operation value of data is " + val);
+    val = operation(val);
+    System.out.println("After operation value of data is " + val);
+  }
+}
